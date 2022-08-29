ba6c30769040b55602f6845f45d495d9211917b3
Han Zhai <zhaihan001@gmail.com>
2022-08-29 16:00:57 -0700

    html file now generated successfully. Will be working on adding additional html sections for engineer and interns

--- generateHTML.js
+++ generateHTML.js
@@ -1,7 +1,47 @@
-function generate(response) {
+function manager(managerInfo) {
+    return `
+        <div class="card">
+            <h3 class="card-header"> ${managerInfo.name}</h3>
+            <h3 class="card-header">Title: ${managerInfo.role}</h3>
+            <div class="card-body">
+                <p class="card-text">ID: ${managerInfo.id}</p>
+                <p class="card-text"><a href="${managerInfo.email}" target="_blank">Email: ${managerInfo.email}</a></p>
+                <p class="card-text">Office: ${managerInfo.officeNumber}</p>
+            </div>
+        </div>`
+}
+
+function intern(internInfo) {
+    for (const info of internInfo){
+        window.write(`
+        <div class="card">
+            <h3 class="card-header"> ${info.name}</h3>
+            <h3 class="card-header">Title: ${info.role}</h3>
+            <div class="card-body">
+                <p class="card-text">ID: ${info.id}</p>
+                <p class="card-text"><a href="${info.email}" target="_blank">Email: ${info.email}</a></p>
+                <p class="card-text">School: ${info.school}</p>
+            </div>
+        </div>`);
+    }
+}
+
+function engineer(engineerInfo) {
+    return `
+        <div class="card">
+            <h3 class="card-header"> ${engineerInfo.name}</h3>
+            <h3 class="card-header">Title: ${engineerInfo.role}</h3>
+            <div class="card-body">
+                <p class="card-text">ID: ${engineerInfo.id}</p>
+                <p class="card-text"><a href="${engineerInfo.email}" target="_blank">Email: ${engineerInfo.email}</a></p>
+                <p class="card-text">Github Username: <a href="https://github.com/${engineerInfo.github}" target="_blank">${engineerInfo.github}</a></p>
+            </div>
+        </div>`
+}
+
+function generateHTML(managerInfo, engineerInfo, internInfo) {
     return `<!DOCTYPE html>
     <html lang="en">
-    
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -14,24 +54,15 @@ function generate(response) {
         <div class="jumbotron jumbotron-fluid">
             <h1 class="display-4">My Team</h1>
         </div>
-    
         <main class="container">
             <section class="row my-4">
-                <div class="card">
-                    <h3 class="card-header name"> ${response.managerName}</h3>
-                    <h3 class="card-header title">Title: Manager</h3>
-                    <div class="card-body">
-                        <p class="card-text id">ID: ${response.managerId}</p>
-                        <p class="card-text email"><a href="${response.managerEmail}">Email: ${response.managerEmail}</a></p>
-                        <p class="card-text office">Office: ${response.managerOffice}</p>
-                    </div>
-                </div>
+            ${manager(managerInfo)}
+            ${engineer(engineerInfo)}
+            ${intern(internInfo)}
             </section>
         </main>
     </body>
     </html>`
 }
 
-module.exports = {
-    generate
-}
+module.exports = generateHTML;
--- index.js
+++ index.js
@@ -1,87 +1,145 @@
 // Include packages needed for this application
 const inquirer = require('inquirer');
-const menu = require('inquirer-menu');
 const fs = require('fs');
 const generateHTML = require('./generateHTML.js');
-const Employee = require('./employee.js');
-
-class Manager extends Employee{
-    constructor(name, id, email, officeNumber){
-        super(name, id, email);
-        this.officeNumber = officeNumber;
-    }
-    getRole(){
-        return 'Manager';
-    }
-}
-
-class Engineer extends Employee{
-    constructor(name, id, email, github){
-        super(name, id, email);
-        this.github = github;
-    }
-    getGithub(){
-
-    }
-    getRole(){
-        return 'Engineer';
-    }
-}
-
-class Intern extends Employee{
-    constructor(name, id, email, school){
-        super(name, id, email);
-        this.github = github;
-    }
-    getSchool(){
-
-    }
-    getRole(){
-        return 'Intern';
-    }
-}
+const Engineer = require('./lib/engineer.js');
+const Intern = require('./lib/intern.js');
+const Manager = require('./lib/manager.js');
+var managerInfo = {};
+var engineerInfo = [];
+var internInfo = [];
 
 // Create an array of questions for user input
-const questions = [
-    {
-        type:'input',
-        message: 'Please enter the name of team manager: ',
-        name:'managerName',
-    },
-    {
-        type:'input',
-        message: 'Please enter the employee ID: ',
-        name:'managerId',
-    },
-    {
-        type:'input',
-        message: 'Please enter the email address: ',
-        name:'managerEmail',
-    },
-    {
-        type:'input',
-        message: 'Please enter the office number: ',
-        name:'managerOffice',
-    },
-    {
-        type: 'list',
-        message: 'Do you want to add a team member',
-        name: 'addMember',
-        choices: ['Yes', 'No'],
-    }
-];
+function addManager() {
+    const managerQuestions = [
+        {
+            type: 'input',
+            message: 'Please enter the name of team manager: ',
+            name: 'managerName',
+        },
+        {
+            type: 'input',
+            message: 'Please enter the employee ID: ',
+            name: 'managerId',
+        },
+        {
+            type: 'input',
+            message: 'Please enter the email address: ',
+            name: 'managerEmail',
+        },
+        {
+            type: 'input',
+            message: 'Please enter the office number: ',
+            name: 'managerOffice',
+        }
+    ]
+    inquirer.prompt(managerQuestions)
+        .then((response) => {
+            managerInfo = new Manager(response.managerName, response.managerId, response.managerEmail, 'Manager', response.managerOffice)
+            return addMember();
+        })
+}
+
+function addMember() {
+    const addQuestions = [
+        {
+            type: 'list',
+            message: 'Do you want to add another team member?',
+            name: 'addMember',
+            choices: ['Add an engineer', 'Add an intern', `No, I'm done`]
+        }
+    ]
+    inquirer.prompt(addQuestions)
+        .then((response) => {
+            switch (response.addMember) {
+
+                case 'Add an engineer':
+                    return addEngineer();
+
+                case 'Add an intern':
+                    return addIntern();
+
+                case `No, I'm done`:
+                    writeToFile(managerInfo, engineerInfo, internInfo);
+                    break;
+            }
+        })
+}
+
+function addEngineer() {
+    const engineerQuestions = [
+        {
+            type: 'input',
+            message: 'Please enter the name of team member: ',
+            name: 'engineerName',
+        },
+        {
+            type: 'input',
+            message: 'Please enter the employee ID: ',
+            name: 'engineerId',
+        },
+        {
+            type: 'input',
+            message: 'Please enter the email address: ',
+            name: 'engineerEmail',
+        },
+        {
+            type: 'input',
+            message: 'Please enter the gitHub username: ',
+            name: 'engineerGithub',
+        }
+    ]
+    inquirer.prompt(engineerQuestions)
+        .then((response) => {
+            const temEngineerinfo = new Engineer(response.engineerName, response.engineerId, response.engineerEmail, 'Engineer', response.engineerGithub)
+            engineerInfo.push(temEngineerinfo);
+            console.log(engineerInfo);
+            return addMember();
+        })
+}
+
+function addIntern() {
+    const internQuestions = [
+        {
+            type: 'input',
+            message: 'Please enter the name of team member: ',
+            name: 'internName',
+        },
+        {
+            type: 'input',
+            message: 'Please enter the employee ID: ',
+            name: 'internId',
+        },
+        {
+            type: 'input',
+            message: 'Please enter the email address: ',
+            name: 'internEmail',
+        },
+        {
+            type: 'input',
+            message: 'Please enter the school name: ',
+            name: 'internSchool',
+        }
+    ]
+    inquirer.prompt(internQuestions)
+        .then((response) => {
+            const tempInterninfo = new Intern(response.internName, response.internId, response.internEmail, 'Intern', response.internSchool)
+            internInfo.push(tempInterninfo);
+            console.log(internInfo);
+            return addMember();
+        })
+}
 
 // Create a function to generate HTML file
-function writeToFile(response) {
-    fs.writeFile("index.html", generateHTML.generate(response), (err) =>
-      err ? console.log(err) : console.log('Team Profile page created')
+function writeToFile(managerInfo, engineerInfo, internInfo) {
+    fs.writeFile('./dist/index.html', generateHTML(managerInfo, engineerInfo, internInfo), (err) =>
+        err ? console.log(err) : console.log('Team Profile page created')
     );
 }
 
 // Create a function to initialize app
 function init() {
-    inquirer.prompt(questions)
-    .then((response) => writeToFile(response));
+    addManager()
 }
 
 // Function call to initialize app
